211c31af5b2ef30d40f9805d8c638d913ed30711
Zaman Hudayberdiyev <zaman.akmyradovic@gmail.com>
2025-05-17 16:01:40 +0500

    add eslint-i18next-plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,23 +2,50 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import i18next from "eslint-plugin-i18next"
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: {
+    parser: tseslint.parser,
+    parserOptions: {
+      project: "./tsconfig.json",
+      ecmaFeatures: {
+        jsx: true,
+      }
+    },
+    globals: globals.browser
+  } },
+
   tseslint.configs.recommended,
   {
-    ...pluginReact.configs.flat.recommended,
+  ...pluginReact.configs.flat.recommended,
     settings: {
       react: {
         version: "detect",
       },
     },
     rules: {
-      "react/react-in-jsx-scope": "off", // Disable React global requirement
+      "react/react-in-jsx-scope": "off",
       "react/jsx-indent": [2,2],
-      "@typescript-eslint/no-unused-vars": "off", // (Optional) Disable unused vars rule
+      "@typescript-eslint/no-unused-vars": "off",
     },
   },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    plugins: {
+      i18next
+    },
+    rules: {
+      "i18next/no-literal-string": [
+          "error",
+        {
+          markupOnly: true,
+          ignoreAttribute: ["data-testid", "to"]
+        }
+      ]
+    }
+  }
 ]);

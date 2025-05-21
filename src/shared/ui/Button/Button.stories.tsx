5eddaf9c4e081563db69d95bf019a8f05802d3d3
Zaman Hudayberdiyev <zaman.akmyradovic@gmail.com>
2025-05-21 16:18:42 +0500

    install storybook

---
+++ src/shared/ui/Button/Button.stories.tsx
@@ -0,0 +1,32 @@
+import type {Meta, StoryObj} from '@storybook/react';
+import {fn} from '@storybook/test';
+
+import {Button, ThemeButton} from './Button';
+
+const meta = {
+    title: 'shared/Button',
+    component: Button,
+    parameters: {
+        layout: 'centered',
+    },
+    tags: ['autodocs'],
+    args: { onClick: fn() },
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Clear: Story = {
+    args: {
+        theme: ThemeButton.CLEAR,
+        children: "Test"
+    },
+};
+
+export const Primary: Story = {
+    args: {
+        theme: ThemeButton.PRIMARY,
+        children: "Test"
+    },
+};
+

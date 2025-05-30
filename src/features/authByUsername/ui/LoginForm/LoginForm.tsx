1ec6b33e6fde4f016961122eccea761b099eb433
Zaman Hudayberdiyev <zaman.akmyradovic@gmail.com>
2025-05-30 13:02:38 +0500

    add DynamicModuleLoader

--- src/features/authByUsername/ui/LoginForm/LoginForm.tsx
+++ src/features/authByUsername/ui/LoginForm/LoginForm.tsx
@@ -3,37 +3,32 @@ import styles from "./LoginForm.module.scss"
 import { useTranslation } from "react-i18next"
 import { Button, ThemeButton } from "~shared/ui/Button/Button"
 import { Input } from "~shared/ui/input/Input"
-import { useSelector, useStore } from "react-redux"
-import { memo, useCallback, useEffect } from "react"
+import { useSelector } from "react-redux"
+import { memo, useCallback } from "react"
 import { loginActions, loginReducer } from "../../model/slice/loginSlice"
 import { loginByUsername } from "~features/authByUsername/model/services/loginByUsername/loginByUsername"
 import { useAppDispatch } from "~shared/hooks/useAppDispatch"
 import { Text, TextTheme } from "~shared/ui/Text/Text"
-import { ReduxStoreWithManager } from "~app/providers/StoreProvider"
 import { getLoginUsername } from "../../model/selectors/getLoginUsername/getLoginUsername"
 import { getLoginLoading } from "../../model/selectors/getLoginLoading/getLoginLoading"
 import { getLoginError } from "../../model/selectors/getLoginError/getLoginError"
 import { getLoginPassword } from "../../model/selectors/getLoginPassword/getLoginPassword"
+import {DynamicModuleLoader, ReducerList} from "~shared/lib/components/DynamicModuleLoader/DynamicModuleLoader";
 
 export interface LoginFormProps {
   className?: string
 }
 
+const initialReducers: ReducerList = {
+    loginForm: loginReducer,
+}
+
 const LoginForm = memo(({ className }: LoginFormProps) => {
   const { t } = useTranslation()
   const dispatch = useAppDispatch()
-  const store = useStore() as ReduxStoreWithManager
 
-  useEffect(() => {
-    store.reducerManager.add("loginForm", loginReducer)
-      dispatch({type: "@INIT loginForm reducer"})
 
-    return () => {
-      store.reducerManager.remove("loginForm")
-        dispatch({type: "@DESTROY loginForm reducer"})
-    }
-    // eslint-disable-next-line
-  }, [])
+
 
   const username = useSelector(getLoginUsername)
   const password = useSelector(getLoginPassword)
@@ -64,10 +59,14 @@ const LoginForm = memo(({ className }: LoginFormProps) => {
   }, [password, username, dispatch])
 
   return (
-    <div className={classNames(styles.LoginForm, {}, [className])}>
-      <Text title={t("Form of Authorization")} />
-      {error && <Text text={error} theme={TextTheme.ERROR} />}
-      <Input
+    <DynamicModuleLoader
+        reducers={initialReducers}
+        removeAfterUnmount
+    >
+      <div className={classNames(styles.LoginForm, {}, [className])}>
+        <Text title={t("Form of Authorization")} />
+        {error && <Text text={error} theme={TextTheme.ERROR} />}
+        <Input
         onChange={onChangeUsername}
         className={styles.input}
         type="text"
@@ -75,22 +74,23 @@ const LoginForm = memo(({ className }: LoginFormProps) => {
         autoFocus
         value={username}
       />
-      <Input
+        <Input
         className={styles.input}
         type="text"
         placeholder={t("Enter Password")}
         onChange={onChangePassword}
         value={password}
       />
-      <Button
+        <Button
         onClick={onLoginClick}
         theme={ThemeButton.OUTLINE}
         className={styles.loginBtn}
         disabled={isLoading}
       >
-        {t("login")}
-      </Button>
-    </div>
+          {t("login")}
+        </Button>
+      </div>
+    </DynamicModuleLoader>
   )
 })
 
--- src/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader.tsx
+++ src/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader.tsx
@@ -1,11 +1,42 @@
-import {FC, ReactNode} from "react";
+import {FC, ReactNode, useEffect} from "react";
+import {useStore} from "react-redux";
+import {ReduxStoreWithManager, StateSchemaKey} from "~app/providers/StoreProvider";
+import {useAppDispatch} from "~shared/hooks/useAppDispatch";
+import {Reducer} from "@reduxjs/toolkit";
+
+export type ReducerList ={
+    [name in StateSchemaKey]?: Reducer
+}
 
 interface DynamicModuleLoaderProps {
     children?: ReactNode
+    reducers: ReducerList
+    removeAfterUnmount?: boolean
 }
 
 export const DynamicModuleLoader: FC<DynamicModuleLoaderProps> = (props ) => {
-    const { children } = props
+    const { children , reducers, removeAfterUnmount} = props
+    const store = useStore() as ReduxStoreWithManager
+    const dispatch = useAppDispatch()
+
+    useEffect(() => {
+        Object.entries(reducers).forEach(([name, reducer]) => {
+            store.reducerManager.add(name as StateSchemaKey, reducer)
+            dispatch({type: `@INIT ${name} reducer`})
+        })
+
+        return () => {
+            if (removeAfterUnmount) {
+                Object.entries(reducers).forEach(([[name]]) => {
+
+            store.reducerManager.remove(name as StateSchemaKey)
+            dispatch({type: `@DESTROY ${name} reducer`})
+                })
+            }
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
     return (
       <>
         {children}
